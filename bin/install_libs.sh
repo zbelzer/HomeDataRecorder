ba449dda1069ff332017e174cf498dad1de78d35
Zachary Belzer <zbelzer@clearstorydata.com>
2015-09-03 22:40:44 -0500

    Fix bug

--- bin/install_libs.sh
+++ bin/install_libs.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-mvn install:install-file -Dfile=libs/xbjlib-1.1.0.jar -DgroupId=com.digi.xbee -DartifactId=xbjlib -Dversion=1.1.0 -Dpackaging=pom -DgeneratePom=true
+mvn install:install-file -Dfile=libs/xbjlib-1.1.0.jar -DgroupId=com.digi.xbee -DartifactId=xbjlib -Dversion=1.1.0 -Dpackaging=jar -DgeneratePom=true
 mvn install:install-file -Dfile=libs/rxtx-2.2.jar -DgroupId=gnu.io.rxtx -DartifactId=rxtx -Dversion=2.2 -Dpackaging=jar -DgeneratePom=true
